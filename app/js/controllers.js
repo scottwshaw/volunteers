c02f4b3ec78fcfced7e2cedc630c36878e9d3735
Scott Shaw <scottwshaw@gmail.com>
2013-06-29 13:29:17 +1000

    added modal for viewing details

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,17 +1,34 @@
 'use strict';
 angular.module('volunteers',['ui.bootstrap']);
+
 /* Controllers */
 function ActivityCtrl($scope) {
     $scope.activities = [
-	{"name": "Jaffle Smash"},
-	{"name": "Curries"},
-	{"name": "Corn on the cob"}
+	{"name": "Jaffle Smash", "owner": {"name": "Geoff", "email": "geoff@somewhere.com"}},
+	{"name": "Curries", "owner": {"name": "Lisa"}},
+	{"name": "Corn on the cob", "owner": {"name": "Shane", "phone": "0414653576"}}
     ];
     $scope.addActivity = function() {
     	$scope.activities.push({"name": $scope.newActivity});
 	$scope.newActivity = "";
     };
+
     $scope.removeActivity = function(activityIndex) {
 	$scope.activities.splice(activityIndex, 1);
     };
-}
+
+    $scope.open = function (activity) {
+	$scope.shouldBeOpen = true;
+	$scope.modalactivity = activity;
+    };
+
+    $scope.close = function () {
+	$scope.shouldBeOpen = false;
+    };
+
+    $scope.opts = {
+	backdropFade: true,
+	dialogFade:true
+    };
+
+};
